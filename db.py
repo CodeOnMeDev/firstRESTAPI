e2875870f89a504d8bad2c822e76c46338884482
Parisa <p.foroutan87@gmail.com>
2023-12-13 12:08:59 -0500

    First API Commit!

---
+++ db.py
@@ -0,0 +1,15 @@
+# stores = {}
+# items = {
+#     1:{
+#         "name": "Chair",
+#         "price":15.99
+#     },
+#     2: {
+#         "name": "Table",
+#         "price":180.5
+#     }
+# }
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
